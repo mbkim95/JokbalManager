94584c4d37d07d72c8335502afd6b64722712ebf
mbkim95 <mbkim95@naver.com>
2021-02-09 02:09:11 +0900

    뷰바인딩 추가

---
+++ app/src/main/java/com/example/jokbalmanager/MainFragment.kt
@@ -0,0 +1,21 @@
+package com.example.jokbalmanager
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.jokbalmanager.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMainBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+}
