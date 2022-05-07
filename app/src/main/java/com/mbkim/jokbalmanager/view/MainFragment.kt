721d9c2de603f9849c31865ffd685348a314b16c
mbkim95 <mbkim95@naver.com>
2022-05-07 21:06:17 +0900

    백업/복구 메뉴 추가

--- app/src/main/java/com/mbkim/jokbalmanager/view/MainFragment.kt
+++ app/src/main/java/com/mbkim/jokbalmanager/view/MainFragment.kt
@@ -1,11 +1,11 @@
 package com.mbkim.jokbalmanager.view
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.google.android.material.tabs.TabLayoutMediator
+import com.mbkim.jokbalmanager.R
 import com.mbkim.jokbalmanager.adapter.TabAdapter
 import com.mbkim.jokbalmanager.databinding.FragmentMainBinding
 
@@ -19,7 +19,13 @@ class MainFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentMainBinding.inflate(layoutInflater, container, false)
+        setHasOptionsMenu(true)
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.title_menu, menu)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -30,6 +36,18 @@ class MainFragment : Fragment() {
                 tab.text = tabNames[position]
             }.attach()
         }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.backup -> {
+                Toast.makeText(requireContext(), "백업하기", Toast.LENGTH_SHORT).show()
+            }
+            R.id.restore -> {
+                Toast.makeText(requireContext(), "복구하기", Toast.LENGTH_SHORT).show()
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onDestroyView() {
